4a43e72de0f2cdddaba0a3153f200db54c76fd3b
서승혁 <wsdvbn011@gmail.com>
2025-11-18 13:36:16 +0900

    edit vite.config

--- my-search/vite.config.js
+++ my-search/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
   plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, 'build'),
+  },
   resolve: {
     dedupe: ['react', 'react-dom', 'styled-components'],
   },
