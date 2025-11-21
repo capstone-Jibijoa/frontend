4c0e3044c69bf158aa2a638c47ad40a5814e771f
서승혁 <wsdvbn011@gmail.com>
2025-11-21 23:18:27 +0900

    test connection

--- my-search/src/pages/ResultsLitePage.jsx
+++ my-search/src/pages/ResultsLitePage.jsx
@@ -69,7 +69,8 @@ const ResultsLitePage = () => {
             }));
 
             try {
-                const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+                //const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+                const API_BASE_URL = 'http://16.176.67.150:8000';
                 const url = `${API_BASE_URL}/api/search`;
                 const body = { query: query };
                 
